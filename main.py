1ff1480c5020cba80c1a2680f4304acdb02d270e
m1nz1k <61007171+m1nz1k@users.noreply.github.com>
2023-10-20 23:14:30 +0700

    del position

--- main.py
+++ main.py
@@ -37,14 +37,15 @@ async def secondary_main():
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
         'content-type': 'application/json;charset=utf-8',
-        'referer': 'https://jetlend.ru/invest/v3/company/16578/loans',
-        'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+        # 'cookie': '_jl_uid=vETZ4WUILvEgftokBER4Ag==; _gcl_au=1.1.1801966748.1695035050; referrer=https://client.work-zilla.com/; _ym_uid=1695035051576708091; _ym_d=1695035051; tmr_lvid=79a9abd783270f5e68cbc6f0ba57bfa2; tmr_lvidTS=1695035055405; _tt_enable_cookie=1; _ttp=rALbsDXw_v8KYn8xszNzyrPqCrx; jl_features=headerFeatures%3Dheader_1_counter%3BfeedbacksFeature%3Ddefault%3BpopUpInvestorFeature%3Ddefault; _gid=GA1.2.244274248.1697817596; _ym_visorc=b; _ym_isad=1; _clck=flvtcj|2|fg0|0|1373; csrftoken=YdMujkr5zKr25QfI5KzAHfwdYC0pGg3O; sessionid=7fhol55ac5e230ill0mtm43cfnx65nry; _ga_NR0DV46HQK=GS1.1.1697817596.31.1.1697817673.50.0.0; _ga=GA1.2.97225234.1695035049; tmr_detect=1%7C1697817673555; _clsk=1qy797u|1697817888574|10|1|i.clarity.ms/collect',
+        'referer': 'https://jetlend.ru/invest/v3/company/13942/analytics',
+        'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
     }
 
     params = {
@@ -363,14 +364,15 @@ async def main():
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
         'content-type': 'application/json;charset=utf-8',
-        'referer': 'https://jetlend.ru/invest/v3/company/16578/loans',
-        'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+        # 'cookie': '_jl_uid=vETZ4WUILvEgftokBER4Ag==; _gcl_au=1.1.1801966748.1695035050; referrer=https://client.work-zilla.com/; _ym_uid=1695035051576708091; _ym_d=1695035051; tmr_lvid=79a9abd783270f5e68cbc6f0ba57bfa2; tmr_lvidTS=1695035055405; _tt_enable_cookie=1; _ttp=rALbsDXw_v8KYn8xszNzyrPqCrx; jl_features=headerFeatures%3Dheader_1_counter%3BfeedbacksFeature%3Ddefault%3BpopUpInvestorFeature%3Ddefault; _gid=GA1.2.244274248.1697817596; _ym_visorc=b; _ym_isad=1; _clck=flvtcj|2|fg0|0|1373; csrftoken=YdMujkr5zKr25QfI5KzAHfwdYC0pGg3O; sessionid=7fhol55ac5e230ill0mtm43cfnx65nry; _ga_NR0DV46HQK=GS1.1.1697817596.31.1.1697817673.50.0.0; _ga=GA1.2.97225234.1695035049; tmr_detect=1%7C1697817673555; _clsk=1qy797u|1697817888574|10|1|i.clarity.ms/collect',
+        'referer': 'https://jetlend.ru/invest/v3/company/13942/analytics',
+        'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
     }
     while True:
         try:
